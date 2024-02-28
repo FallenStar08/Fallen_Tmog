a9e49b9d087db7870091ad703ad728ff8fcb6631
FallenStar <23637993+FallenStar08@users.noreply.github.com>
2024-02-29 00:28:17 +0100

    Add uninstall command

--- Fallen_Tmog/Mods/Fallen_Tmog/ScriptExtender/Lua/Server/main.lua
+++ Fallen_Tmog/Mods/Fallen_Tmog/ScriptExtender/Lua/Server/main.lua
@@ -30,18 +30,20 @@ local function updatePotionsDescription()
     -- Translated strings for visibility
     local visibleText = GetTranslatedString(Handles["Visible"]) or "Visible"
     local invisibleText = GetTranslatedString(Handles["Invisible"]) or "Invisible"
-    
+
     local greenHex = "#00FF00"  -- Green
-    local orangeHex = "#FFA500"  -- Orange
-    
+    local orangeHex = "#FFA500" -- Orange
+
     -- Generate description content for each description slot
     for _, slot in pairs(descriptionSlots) do
         local slotDescriptionHandle = SlotToDescriptionHandle[slot]
         descriptionContent[slot] = ""
         for _, member in pairs(SQUADIES) do
             local visibility = result[member] and result[member][slot]
-            local translatedVisibility = visibility and ColorTranslatedString(invisibleText, orangeHex) or ColorTranslatedString(visibleText, greenHex)
-            descriptionContent[slot] = descriptionContent[slot] .. GetTranslatedName(member) .. " : " .. translatedVisibility .. "\n"
+            local translatedVisibility = visibility and ColorTranslatedString(invisibleText, orangeHex) or
+            ColorTranslatedString(visibleText, greenHex)
+            descriptionContent[slot] = descriptionContent[slot] ..
+            GetTranslatedName(member) .. " : " .. translatedVisibility .. "\n"
         end
         UpdateTranslatedString(Handles[slotDescriptionHandle], descriptionContent[slot])
     end
@@ -260,3 +262,28 @@ end
 Ext.Osiris.RegisterListener("LevelGameplayStarted", 2, "after", start)
 
 Ext.Events.ResetCompleted:Subscribe(start)
+
+local function uninstall()
+    BasicPrint("uninstall() - Deleting all mod items for the mod 'cosmetic slots'")
+    local itemsToDelete = {}
+    for item, _ in pairs(modItemRoots) do
+        itemsToDelete[#itemsToDelete + 1] = item
+    end
+    for _, member in pairs(GetEveryoneThatIsRelevant()) do
+        local result = DeepIterateInventory(_GE(member), { FilterByTemplate(itemsToDelete) })
+        for uuid, item in pairs(result) do
+            if Osi.IsContainer(uuid) == 1 then
+                BasicDebug("uninstall() - This is a container OwO we need to get stuff out of it just in case OwO")
+                local thingsToGetOut=DeepIterateInventory(_GE(uuid))
+                for thinguuid,_ in pairs(thingsToGetOut) do
+                    Osi.TeleportTo(thinguuid, member)
+                end
+                DelayedCall(200, function() Osi.RequestDelete(uuid) end)
+            else
+                Osi.RequestDelete(uuid)
+            end
+        end
+    end
+end
+
+Ext.RegisterConsoleCommand("fallen_uninstall_cslot", uninstall)
